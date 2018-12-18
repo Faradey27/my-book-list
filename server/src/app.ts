0c28cee4a4902153c1c8e8db866f9a33372c08ad
Faradey27 <andrii.tiertyshnyi@gmail.com>
2018-12-19 00:33:07 +0200

    [server]: Basic integration with mongodb

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,18 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as mongoose from "mongoose";
 
 import { BookRoutes } from './features/books';
 
 class App {
   public app: express.Application;
   public router: BookRoutes = new BookRoutes();
+  public mongoUrl: string = 'mongodb://localhost/mybooklistdb';
 
   constructor() {
     this.app = express();
     this.config();
+    this.mongoSetup();
     this.router.routes(this.app);
   }
 
@@ -18,6 +21,11 @@ class App {
     this.app.use(bodyParser.json());
     //support application/x-www-form-urlencoded post data
     this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+  private mongoSetup(): void{
+    (mongoose as any).Promise = global.Promise;
+    mongoose.connect(this.mongoUrl);
   }
 }
 
